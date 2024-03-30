7e37b1a911fedfba253c5148a210e07d51d745ff
polymersecret <seanmoore.007@outlook.com>
2024-03-30 12:12:20 +0800

    ibc

---
+++ README.md
@@ -0,0 +1,34 @@
+### Assets: 
+```
+innocent merit harbor rifle autumn cart corn wonder voice forward plastic embody
+```
+## Run-book
+1. Contract install, compile, deploy
+```
+just install
+npx hardhat compile
+just deploy optimism base
+```
+2. Copy the result of contract deployed(port address) or go to /config/config.json copy sendUniversalPacket.optimism.portAddr
+3. Go to /ibc-token-bridge/src/App.vue replace `CONTRACT_ADDRESS` with port address from step 2
+4. Go to /artifacts/contracts/IBCToken.sol/IBCToken.json file to copy ABI
+5. Go to /ibc-token-bridge/src/abi.js replace `ACONTRACT_ABIBI` with ABI from setp 4
+6. Frontend install, start
+```
+cd ibc-token-bridge
+npm i
+npm run serve
+```
+
+## Tx hash
+Optimism tx hash: 0x27a859bc3038ee5bbe90c71c39007005732ca33a355181cd3bef3ac779e3f736
+Base tx hash: 0xe206968e327389e3c1cf18e6dcf1b6503b97f72fee31a6ab15f7b41ce5881d5c
+
+
+## PortAddress
+Op: 0xc958C5Ed8f1deAc4B1763889a45a6a24d9EF3871
+Base: 0xc958C5Ed8f1deAc4B1763889a45a6a24d9EF3871
+
+
+
+
---
+++ scripts/bridge-token.js
@@ -0,0 +1,31 @@
+const Web3 = require('web3').default;
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const contractInfro = require('../artifacts/contracts/IBCToken.sol/IBCToken.json');
+const configInfro = require('../config/config.json');
+
+async function main(){
+  try{
+const mnemonic = 'innocent merit harbor rifle autumn cart corn wonder voice forward plastic embody';
+    const chain = process.argv[2];
+    const amountSmall = process.argv[3];
+    let amount = amountSmall * 1000000000000000000
+    let url = (chain === 'optimism' ? 'https://sepolia.optimism.io': 'https://sepolia.base.org')
+    const provider = new HDWalletProvider(mnemonic, url);
+    const web3 = new Web3(provider);
+    const contract = new web3.eth.Contract(contractInfro.abi, configInfro.sendUniversalPacket[process.argv[2]].portAddr);
+    const channel = web3.utils.padRight(web3.utils.asciiToHex(configInfro.sendUniversalPacket[process.argv[2]].channelId), 64);
+    const method = contract.methods.crossChainTransfeer(configInfro.sendUniversalPacket[process.argv[2]].portAddr, channel, amount);
+    const account = (await web3.eth.getAccounts())[0];
+    const tx = method.send({ from: account });
+    tx.on('transactionHash', (hash) => {
+      console.log('tx hash:', hash);
+    });
+  }catch(e){
+    console.error(e)
+  }
+    
+}
+main().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
